44a884f81fa492b971633fe36699bd1acf6078f3
MuyiwaOkee <dj_productions@outlook.com>
2025-06-14 21:43:27 +0100

    code refactor

--- src/components/Auth/action.ts
+++ src/components/Auth/action.ts
@@ -70,69 +70,73 @@ export type SignupOutput = {
     }
 }
 
-export const Signup = async ({ section }: { section: "EmailPassword" | "UserRole"}, formData: FormData):Promise<SignupOutput> =>  {
+export const Signup = async ({ section }: SignupOutput, formData: FormData):Promise<SignupOutput> =>  {
     const formDataObject = Object.fromEntries(formData) as SignupType;
 
     // find the section of the signup process there at, and then run the processes for that section
-    if(section === 'EmailPassword') {
-        const result = loginSchema.safeParse(formDataObject);
+    switch (section) {
+        case 'EmailPassword':
+            const result = loginSchema.safeParse(formDataObject);
 
-        if(!result.success) {
-            return {
-                section: 'EmailPassword',
-                errors: result.error.flatten().fieldErrors
-            };
-        }
-
-        return {
-            section: 'UserRole',
-            errors: {
-                email: undefined,
-                password: undefined
+            if(!result.success) {
+                return {
+                    section: 'EmailPassword',
+                    errors: result.error.flatten().fieldErrors
+                };
             }
-        }
-    } else if (section === 'UserRole') {
-        // first, check if a role has sucessfully been chosen
-        const result1 = userRoleSchema.safeParse(formDataObject.roleCombobox);
 
-        if(!result1.success) {
             return {
                 section: 'UserRole',
                 errors: {
-                    roleCombobox: ['Please select a valid role'],
-                    sectorCombobox: undefined
+                    email: undefined,
+                    password: undefined
                 }
             }
-        }
 
-        // if the chosen type was business, check for a valid sector
-        if(formDataObject.roleCombobox === 'Business') {
-            const result2 = sectorSchema.safeParse(formDataObject.sectorCombobox);
+        case 'UserRole':
+            // first, check if a role has sucessfully been chosen
+            const result1 = userRoleSchema.safeParse(formDataObject.roleCombobox);
 
-            if(!result2.success) {
+            if(!result1.success) {
                 return {
                     section: 'UserRole',
                     errors: {
-                        roleCombobox: undefined,
-                        sectorCombobox: ['Please select a valid sector']
+                        roleCombobox: ['Please select a valid role'],
+                        sectorCombobox: undefined
                     }
                 }
             }
-        }
 
-        // Create the user account in the database and create session
-        const { email, password: passwordNotHashed, roleCombobox: role, sectorCombobox:sector } = formDataObject;
+            // if the chosen type was business, check for a valid sector
+            if(formDataObject.roleCombobox === 'Business') {
+                const result2 = sectorSchema.safeParse(formDataObject.sectorCombobox);
 
-        const newUser = {
-            email,
-            password: await hash(passwordNotHashed!), //hashed and salted password for added security
-            role,
-            sector
-        };
+                if(!result2.success) {
+                    return {
+                        section: 'UserRole',
+                        errors: {
+                            roleCombobox: undefined,
+                            sectorCombobox: ['Please select a valid sector']
+                        }
+                    }
+                }
+            }
 
-        redirect('/dashboard');
-    } else {
-        return {
+            // Create the user account in the database and create session
+            const { email, password: passwordNotHashed, roleCombobox: role, sectorCombobox:sector } = formDataObject;
+
+            // this will be sent via post request to create a new user. it should return the user id, which will then be used to create a session before directing to the dashboard
+            const newUser = {
+                email,
+                password: await hash(passwordNotHashed!), //hashed and salted password for added security
+                role,
+                sector
+            };
+
+            redirect('/dashboard');
+    
+        default:
+             return {
                 section: 'EmailPassword',
                 errors: {
                     email: ['Unknown section'],
