f082a7f50904174bd3f7a39d127b807e36f3974b
MuyiwaOkee <dj_productions@outlook.com>
2025-06-20 20:05:54 +0100

    added modal to trends

--- src/app/(main)/dashboard/@trends/page.tsx
+++ src/app/(main)/dashboard/@trends/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 import { DefaultContentCard, IconButtonProps } from '@/components/DefaultContentCard'
+import TextModal, { TextModalRef } from '@/components/TextModal'
 import { TopTooltip } from '@/components/Tooltip'
 import { VisaFileUploadTiny, VisaInformationAltHigh } from '@visa/nova-icons-react'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const TrendsModal = () => {
+  const modalRef = useRef<TextModalRef>(null);
+  
   const cardText = "As of mid-2025, global economic growth is modest, with easing inflation and high interest rates. Developed economies are stabilizing, while emerging markets face mixed conditions. China is recovering slowly. Geopolitical tensions and uncertain demand continue to challenge supply chains, keeping the outlook cautious but less recession-prone than before."
 
   const iconButtons:IconButtonProps[] = [
@@ -23,10 +26,22 @@ const TrendsModal = () => {
   ]
 
   return (
-    <DefaultContentCard id='trends-card' headline='Trend Summary' text={cardText} primaryButton={{
+    <>
+      <DefaultContentCard id='trends-card' headline='Trend Summary' text={cardText} primaryButton={{
       text: 'click me',
-      onClickFunc: () => console.log('trend summary btn was clicked')
+      onClickFunc: () => modalRef.current?.toggleModal(true)
     }} iconButtons={iconButtons}/>
+    <TextModal ref={modalRef} stateClass={{
+        state: 'viewing',
+        message: 'textMsg',
+      }} 
+      primaryButton={{
+        text: 'Hello',
+        icon: <VisaFileUploadTiny />
+      }} secondaryButton={{
+        text: 'Hello'
+      }} notificationTitle='Report & Next Steps'/>
+    </>
   )
 }
 
--- src/app/(main)/predict/page.tsx
+++ src/app/(main)/predict/page.tsx
@@ -1,32 +1,15 @@
-"use client"
-import TextModal, { TextModalRef } from '@/components/TextModal';
-import { VisaFileUploadTiny } from '@visa/nova-icons-react';
 import { Metadata } from 'next';
-import React, { useRef } from 'react'
+import React from 'react'
 
-// export const metadata: Metadata = {
-//   title: "Predict",
-//   description: "Prediction page",
-// };
+export const metadata: Metadata = {
+  title: "Predict",
+  description: "Prediction page",
+};
 
 const PreditionPage = () => {
-  const textMsg = 'Hello';
-
-  const modalRef = useRef<TextModalRef>(null);
-
   return (
     <section className='w-full'>
-      <button onClick={() => modalRef.current?.toggleModal(true)}>Click me pls</button>
-      <TextModal ref={modalRef} stateClass={{
-        state: 'viewing',
-        message: textMsg,
-      }} 
-      primaryButton={{
-        text: 'Hello',
-        icon: <VisaFileUploadTiny />
-      }} secondaryButton={{
-        text: 'Hello'
-      }} notificationTitle='Report & Next Steps'/>
+      hello
     </section>
   )
 }
